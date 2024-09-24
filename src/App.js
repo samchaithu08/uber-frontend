83ac44fac6492bd20f9e07f6b17143f8c7088646
atulit gaur <atulit23@gmail.com>
2024-09-24 22:37:43 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css"; // Optional for additional styling
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  // State variables to store the results
+  const [result1, setResult1] = useState("");
+  const [result2, setResult2] = useState("");
+  const [result3, setResult3] = useState("");
+  const [result4, setResult4] = useState("");
+  const [result5, setResult5] = useState("");
+
+  // Function to fetch data from the API and set the corresponding state
+  const fetchData = async (endpoint, setResult) => {
+    try {
+      const response = await axios.get(endpoint);
+      setResult(JSON.stringify(response.data, null, 2));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setResult("Error fetching data");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="text-center">Uber Data Analysis</h1>
+      <div className="row mt-4">
+        <div className="col-md-6 mb-4">
+          <div style={{display: 'flex', marginBottom: '2rem'}}>
+            <h3 style={{marginRight: '2rem'}}>Get analytics</h3>
+            <button
+              className="btn btn-primary"
+              onClick={() =>
+                fetchData("http://44.193.195.188/fetch-analytics", setResult1)
+              }
+            >
+              Get Results
+            </button>
+          </div>
+          <pre>{result1}</pre>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
